5a2f80d2573e66a6ba04639e9fdf6974584b5277
SuperJappie08 <36795178+SuperJappie08@users.noreply.github.com>
2025-08-21 16:12:34 +0200

    Prevent double publishing of encoder messages
    Signed-off-by: SuperJappie08 <36795178+SuperJappie08@users.noreply.github.com>

--- mirte_telemetrix_cpp/src/sensors/encoder_monitor.cpp
+++ mirte_telemetrix_cpp/src/sensors/encoder_monitor.cpp
@@ -32,8 +32,8 @@ EncoderMonitor::EncoderMonitor(NodeData node_data, EncoderData encoder_data)
 
 void EncoderMonitor::data_callback(int16_t value) {
   this->value += value;
-  this->update();
-  this->device_timer->reset();
+  // this->update();
+  // this->device_timer->reset();
 }
 
 void EncoderMonitor::update() {
